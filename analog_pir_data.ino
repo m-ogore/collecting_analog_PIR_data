2c9ef19bcd6787ddf1a087be215d5d9fd87ba2ea
m-ogore <42109589+m-ogore@users.noreply.github.com>
2022-08-22 12:03:01 +0200

    Update analog_pir_data.ino

--- analog_pir_data.ino
+++ analog_pir_data.ino
@@ -1,12 +1,7 @@
 #include <SPI.h> //Library for SPI communication (Pre-Loaded into Arduino)
 
-#define TdsSensorPin A7
-
-
-
-#define pirPin A0
-const int chipSelect = 10; //SD card CS pin connected to pin 4 of Arduino
-float pirvalue;
+#define IN_PIN A0
+#define WINDOW_SIZE 5
 
 /////////////////////////////////////////////////////////////////
 //#define FREQUENCY_HZ        50
@@ -16,34 +11,42 @@ float pirvalue;
 
 static unsigned long last_interval_ms = 0;
 
+int INDEX = 0;
+int VALUE = 0;
+int SUM = 0;
+int READINGS[WINDOW_SIZE];
+int AVERAGED = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  // Setup Serial connection
+  pinMode(IN_PIN, INPUT);
   Serial.begin(9600);
-  pinMode(pirPin, INPUT);
-
 }
 
-void reading_pir() {
-  // Reading pir values takes 
-  
-  pirvalue = analogRead(pirPin);
- 
-  
-  // Check if any reads failed and exit early (to try again).
+void loop() {
+    // Check if any reads failed and exit early (to try again).
   if  (isnan(pirvalue) ) {
     Serial.println("Failed to read from sensor!");
     return;
   }
-  
-  //debugging purposes
-  Serial.print("pir value: "); 
-  Serial.println(pirvalue);
-  Serial.println(" ");
-}
+  // Reading pir values takes
+  SUM = SUM - READINGS[INDEX];       // Remove the oldest entry from the sum
+  VALUE = analogRead(IN_PIN);        // Read the next sensor value
+  READINGS[INDEX] = VALUE;           // Add the newest reading to the window
+  SUM = SUM + VALUE;                 // Add the newest reading to the sum
+  INDEX = (INDEX+1) % WINDOW_SIZE;   // Increment the index, and wrap to 0 if it exceeds the window size
 
-void loop() {
+  AVERAGED = SUM / WINDOW_SIZE;      // Divide the sum of the window by the window size for the result
+
+  Serial.print(VALUE);
+  Serial.print(",");
+  Serial.println(AVERAGED);
+  
+  delay(25); 
+ 
+  
+
+
+
     //static unsigned long last_interval_ms = 0;
 
     //if (millis() > last_interval_ms + INTERVAL_MS) {
@@ -51,6 +54,6 @@ void loop() {
        
 
   // put your main code here, to run repeatedly:
-reading_pir();
+
 //}
 }

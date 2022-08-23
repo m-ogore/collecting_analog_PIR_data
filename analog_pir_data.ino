2e49db62ea569594d7b709cbece7f0d0acf9df36
m-ogore <42109589+m-ogore@users.noreply.github.com>
2022-08-23 15:33:39 +0200

    Addedd SD card code

--- analog_pir_data.ino
+++ analog_pir_data.ino
@@ -1,59 +1,87 @@
 #include <SPI.h> //Library for SPI communication (Pre-Loaded into Arduino)
+#include <SD.h> // for the SD card
 
-#define IN_PIN A0
-#define WINDOW_SIZE 5
+
+
+#define pirPin A0
+const int chipSelect = 10; //SD card CS pin connected to pin 4 of Arduino
+float pirvalue;
 
 /////////////////////////////////////////////////////////////////
-//#define FREQUENCY_HZ        50
-//#define INTERVAL_MS         (100000 / (FREQUENCY_HZ + 1))
+#define FREQUENCY_HZ        166
+#define INTERVAL_MS         (100000 / (FREQUENCY_HZ + 1))
 /////////////////////////////////////////////////////////////////
 
+// Create a file to store the data
+File myFile;
 
 static unsigned long last_interval_ms = 0;
 
-int INDEX = 0;
-int VALUE = 0;
-int SUM = 0;
-int READINGS[WINDOW_SIZE];
-int AVERAGED = 0;
 
 void setup() {
-  pinMode(IN_PIN, INPUT);
+  // put your setup code here, to run once:
+  // Setup Serial connection
   Serial.begin(9600);
+  pinMode(pirPin, INPUT);
+
+    // setup for the SD card
+  Serial.print("Initializing SD card...");
+
+  if(!SD.begin(chipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+    
+  //open file
+  myFile=SD.open("DATA.txt", FILE_WRITE);
+
+  // if the file opened ok, write to it:
+  if (myFile) {
+    Serial.println("File opened ok");
+    // print the headings for our data
+    myFile.println("Date,Time,pir values");
+  }
+  myFile.close();
+
 }
 
-void loop() {
-    // Check if any reads failed and exit early (to try again).
+void reading_pir() {
+  // Reading pir values takes 
+  
+  pirvalue = analogRead(pirPin);
+ 
+  
+  // Check if any reads failed and exit early (to try again).
   if  (isnan(pirvalue) ) {
     Serial.println("Failed to read from sensor!");
     return;
   }
-  // Reading pir values takes
-  SUM = SUM - READINGS[INDEX];       // Remove the oldest entry from the sum
-  VALUE = analogRead(IN_PIN);        // Read the next sensor value
-  READINGS[INDEX] = VALUE;           // Add the newest reading to the window
-  SUM = SUM + VALUE;                 // Add the newest reading to the sum
-  INDEX = (INDEX+1) % WINDOW_SIZE;   // Increment the index, and wrap to 0 if it exceeds the window size
-
-  AVERAGED = SUM / WINDOW_SIZE;      // Divide the sum of the window by the window size for the result
-
-  Serial.print(VALUE);
-  Serial.print(",");
-  Serial.println(AVERAGED);
   
-  delay(25); 
- 
-  
+  //debugging purposes
+  Serial.print("pir value: "); 
+  Serial.println(pirvalue);
+  Serial.println(" ");
 
 
+    myFile = SD.open("DATA.txt", FILE_WRITE);
+  if (myFile) {
+    Serial.println("open with success");
+    myFile.print(pirvalue);
+    myFile.println(",");
+  }
+  myFile.close();
+}
 
-    //static unsigned long last_interval_ms = 0;
 
-    //if (millis() > last_interval_ms + INTERVAL_MS) {
-       // last_interval_ms = millis();
+void loop() {
+    static unsigned long last_interval_ms = 0;
+
+    if (millis() > last_interval_ms + INTERVAL_MS) {
+        last_interval_ms = millis();
        
 
   // put your main code here, to run repeatedly:
-
-//}
+reading_pir();
+}
 }

3efeee4e8772791c68d35383241899799971a10d
m-ogore <42109589+m-ogore@users.noreply.github.com>
2022-08-23 12:35:52 +0200

    Add files via upload
    This code works without SD card or timestamp. We get a more reasonable sequence of data from the PIR sensor. Data is read every 6 ms, making our frequency 166HZ

---
+++ tuesda.ino
@@ -0,0 +1,56 @@
+#include <SPI.h> //Library for SPI communication (Pre-Loaded into Arduino)
+
+#define TdsSensorPin A7
+
+
+
+#define pirPin A0
+const int chipSelect = 10; //SD card CS pin connected to pin 4 of Arduino
+float pirvalue;
+
+/////////////////////////////////////////////////////////////////
+#define FREQUENCY_HZ        166
+#define INTERVAL_MS         (100000 / (FREQUENCY_HZ + 1))
+/////////////////////////////////////////////////////////////////
+
+
+static unsigned long last_interval_ms = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  // Setup Serial connection
+  Serial.begin(9600);
+  pinMode(pirPin, INPUT);
+
+}
+
+void reading_pir() {
+  // Reading pir values takes 
+  
+  pirvalue = analogRead(pirPin);
+ 
+  
+  // Check if any reads failed and exit early (to try again).
+  if  (isnan(pirvalue) ) {
+    Serial.println("Failed to read from sensor!");
+    return;
+  }
+  
+  //debugging purposes
+  Serial.print("pir value: "); 
+  Serial.println(pirvalue);
+  Serial.println(" ");
+}
+
+void loop() {
+    static unsigned long last_interval_ms = 0;
+
+    if (millis() > last_interval_ms + INTERVAL_MS) {
+        last_interval_ms = millis();
+       
+
+  // put your main code here, to run repeatedly:
+reading_pir();
+}
+}
